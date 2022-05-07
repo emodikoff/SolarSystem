88eb66e3007c101381128dd3a7ec09987ec1a0a3
Emiliyan Dikov <93236586+emodikoff@users.noreply.github.com>
2022-05-07 23:41:59 +0300

    Add files via upload

---
+++ SolarSystem/src/dikov/Main.java
@@ -0,0 +1,99 @@
+package dikov;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    private static Map<HeavenlyBody.Key, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Set<HeavenlyBody> planets = new HashSet<>();
+
+    public static void main(String[] args) {
+        HeavenlyBody temp = new Planet("Mercury", 88);
+        solarSystem.put(temp.getKey(), temp);
+        planets.add(temp);
+
+        temp = new Planet("Venus", 225);
+        solarSystem.put(temp.getKey(), temp);
+        planets.add(temp);
+
+        temp = new Planet("Earth", 365);
+        solarSystem.put(temp.getKey(), temp);
+        planets.add(temp);
+
+        HeavenlyBody tempMoon = new Moon("Moon", 27);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
+        temp.addSatellite(tempMoon);
+
+        //Mars and his moons
+        temp = new Planet("Mars", 687);
+        solarSystem.put(temp.getKey(), temp);
+        planets.add(temp);
+
+        tempMoon = new Moon("Deimos", 1.3);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
+        temp.addSatellite(tempMoon);
+
+        tempMoon = new Moon("Phobos", 0.3);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
+        temp.addSatellite(tempMoon);
+
+        //Jupiter and his moons
+        temp = new Planet("Jupiter", 4332);
+        solarSystem.put(temp.getKey(), temp);
+        planets.add(temp);
+
+        tempMoon = new Moon("Io", 1.8);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
+        temp.addSatellite(tempMoon);
+
+        tempMoon = new Moon("Europa", 3.5);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
+        temp.addSatellite(tempMoon);
+
+        tempMoon = new Moon("Ganymede", 7.1);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
+        temp.addSatellite(tempMoon);
+
+        tempMoon = new Moon("Callisto", 16.7);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
+        temp.addSatellite(tempMoon);
+
+        //Saturn and his moons
+        temp = new Planet("Saturn", 10759);
+        solarSystem.put(temp.getKey(), temp);
+        planets.add(temp);
+
+        //Uranus and his moons
+        temp = new Planet("Uranus", 30660);
+        solarSystem.put(temp.getKey(), temp);
+        planets.add(temp);
+
+        //Neptune and his moons
+        temp = new Planet("Neptune", 60182);
+        solarSystem.put(temp.getKey(), temp);
+        planets.add(temp);
+
+        temp = new DwarfPlanet("Pluto", 248);
+        solarSystem.put(temp.getKey(), temp);
+        planets.add(temp);
+
+        System.out.println("Planets");
+        for (HeavenlyBody planet : planets) {
+            System.out.println("\t" + planet.getKey());
+        }
+
+        HeavenlyBody body = solarSystem.get(HeavenlyBody.makeKey("Mars", HeavenlyBody.BodyTypes.PLANET));
+        System.out.println("Moons of " + body.getKey());
+        for (HeavenlyBody moons : body.getSatellites()) {
+            System.out.println("\t" + moons.getKey());
+        }
+
+        System.out.println();
+        System.out.println("The solar system contains: ");
+        for (HeavenlyBody heavenlyBody : solarSystem.values()) {
+            System.out.println(heavenlyBody);
+        }
+    }
+}
